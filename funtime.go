99846cc7d14901822d5ccbd44b0ae511e20b3161
Brandon Stewart <brandonstewart300055@gmail.com>
2018-11-20 19:36:38 -0700

    first commit

---
+++ funtime.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+// comment
+
+/*
+
+lots of comments
+
+see https://www.youtube.com/watch?v=CF9S4QZuV30
+
+*/
+
+//functions
+func main()  {
+
+
+  //print statements
+  fmt.Println("Hellow World")
+
+  //varibale declarations
+  var age int = 21
+
+  //floats are float32 and float64
+  var favNum float64 = 3.1415926535
+
+  randNum := 1
+
+  fmt.PrintLn(randNum)
+
+
+
+}
